3442932f0bd02d88453dc94421d88ddf7ef75e59
nikhil <nikhilsai.m23@iiits.in>
2025-03-12 15:22:49 +0530

    added mobiles

--- public/scripts/buy-mobile-data.js
+++ public/scripts/buy-mobile-data.js
@@ -1,26 +1,5 @@
 const mobilePhones = [
-    {
-        id: 1,
-        brand: "Apple",
-        model: "iPhone 14",
-        color: "Midnight",
-        image: "images/buy-page-phones/image_name",
-        specs: {
-            processor: "A15 Bionic",
-            display: "6.1-inch Super Retina XDR",
-            battery: 3279,
-            camera: "12MP + 12MP | 12MP Front Camera",
-            os: "iOS 16",
-            network: "5G",
-            weight: "172g"
-        },
-        ram: "4GB",
-        rom: "128GB",
-        price: 79999,
-        discount: 10, // 10% off
-        condition: "Superb" // Condition added
-    }
-    ,  {
+      {
         id: 0,
         brand: "Apple",
         model: "iPhone 16 (Black, 128 GB)",
@@ -38,14 +17,14 @@ const mobilePhones = [
         ram: "6GB",
         rom: "128GB",
         price: 71999,
-        discount: 15, // 15% off
+        discount: 15, 
         condition: "very Good" // Condition added
     }
     ,  {
         id: 0,
         brand: "Apple",
         model: "iPhone 16 (White, 256 GB)",
-        color: "Black",
+        color: "white",
         image: "images/buy-page-phones/i-16-white.webp",
         specs: {
             processor: "A18 Chip, 6 Core Processor",
@@ -56,31 +35,326 @@ const mobilePhones = [
             network: "5G",
             weight: "200g"
         },
-        ram: "12GB",
+        ram: "8GB",
         rom: "256GB",
         price: 74999,
-        discount: 20, // 15% off
+        discount: 20, 
         condition: "Good" // Condition added
     }
     ,  {
         id: 0,
         brand: "Apple",
         model: "iPhone 13 (Green, 512 GB)",
-        color: "Black",
-        image: "images/buy-page-phones/i-16-black.webp",
+        color: "Green",
+        image: "images/buy-page-phones/i-13.webp",
         specs: {
-            processor: "A18 Chip, 6 Core Processor",
-            display: "15.49 cm (6.1 inch) OLED Display",
-            battery: 5000,
-            camera: "48MP + 12MP | 12MP Front Camera",
-            os: "   iOS 18",
+            processor: "A15 Bionic Chip Processor",
+            display: "15.49 cm (6.1 inch) Super Retina XDR Display",
+            battery: 4200,
+            camera: "12MP + 12MP | 12MP Front Camera",
+            os: "iOS 15",
             network: "5G",
-            weight: "200g"
+            weight: "170g"
         },
         ram: "6GB",
-        rom: "1GB",
+        rom: "512GB",
         price: 74999,
-        discount: 10, // 15% off
+        discount: 10, 
+        condition: "Superb" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 15 Plus (Pink, 128 GB)",
+        color: "Pink",
+        image: "images/buy-page-phones/i-15plus.webp",
+        specs: {
+            processor: "A16 Bionic Chip,6 core Processor",
+            display: "17.02 cm (6.7 inch) Super Retina XDR OLED Display",
+            battery: 4700,
+            camera: "48MP + 12MP | 12MP Front Camera",
+            os: "iOS 17",
+            network: "5G",
+            weight: "201g"
+        },
+        ram: "6GB",
+        rom: "128GB",
+        price: 70999,
+        discount: 5, 
+        condition: "Superb" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 15 Plus (Pink, 256 GB)",
+        color: "Pink",
+        image: "images/buy-page-phones/i-15plus.webp",
+        specs: {
+            processor: "A16 Bionic Chip,6 core Processor",
+            display: "17.02 cm (6.7 inch) Super Retina XDR OLED Display",
+            battery: 4700,
+            camera: "48MP + 12MP | 12MP Front Camera",
+            os: "iOS 17",
+            network: "5G",
+            weight: "201g"
+        },
+        ram: "8GB",
+        rom: "256GB",
+        price: 73999,
+        discount: 5, 
+        condition: "Superb" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 15 Plus (Green, 128 GB)",
+        color: "Green",
+        image: "images/buy-page-phones/i-15plus-green.webp",
+        specs: {
+            processor: "A16 Bionic Chip,6 core Processor",
+            display: "17.02 cm (6.7 inch) Super Retina XDR OLED Display",
+            battery: 4700,
+            camera: "48MP + 12MP | 12MP Front Camera",
+            os: "iOS 17",
+            network: "5G",
+            weight: "201g"
+        },
+        ram: "6GB",
+        rom: "128GB",
+        price: 70999,
+        discount: 5, 
+        condition: "Superb" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 14 (Starlight, 256 GB)",
+        color: "Starlight",
+        image: "images/buy-page-phones/i-14.webp",
+        specs: {
+            processor: "A15 Bionic Chip,6 core Processor",
+            display: "15.49 cm (6.1 inch) Super Retina XDR Display",
+            battery: 3279,
+            camera: "12MP + 12MP | 12MP Front Camera",
+            os: "iOS 16",
+            network: "5G",
+            weight: "172g"
+        },
+        ram: "6GB",
+        rom: "256GB",
+        price: 64999,
+        discount: 8, 
+        condition: "very Good" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 16e (White, 128 GB)",
+        color: "White",
+        image: "images/buy-page-phones/i-16e.webp",
+        specs: {
+            processor: "A18 Bionic Chip,6 core Processor",
+            display: "15.49 cm (6.1 inch) Super Retina XDR OLED Display",
+            battery: 3279,
+            camera: "48MP Rear Camera | 12MP Front Camera",
+            os: "iOS 18",
+            network: "5G",
+            weight: "167g"
+        },
+        ram: "6GB",
+        rom: "128GB",
+        price: 59999,
+        discount: 6, 
+        condition: "Superb" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 16 Plus (White, 128 GB)",
+        color: "White",
+        image: "images/buy-page-phones/i-16plus-white.webp",
+        specs: {
+            processor: "A18 Bionic Chip,6 core Processor",
+            display: "17.02 cm (6.7 inch) Super Retina XDR OLED Display",
+            battery: 3279,
+            camera: "48MP + 12MP Rear | 12MP Front Camera",
+            os: "iOS 18",
+            network: "5G",
+            weight: "199g"
+        },
+        ram: "6GB",
+        rom: "128GB",
+        price: 78999,
+        discount: 6, 
+        condition: "Superb" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 16 Plus (Ultramarine, 128 GB)",
+        color: "Ultramarine",
+        image: "images/buy-page-phones/i-16plus-marine.webp",
+        specs: {
+            processor: "A18 Bionic Chip,6 core Processor",
+            display: "17.02 cm (6.7 inch) Super Retina XDR OLED Display",
+            battery: 3279,
+            camera: "48MP + 12MP Rear | 12MP Front Camera",
+            os: "iOS 18",
+            network: "5G",
+            weight: "199g"
+        },
+        ram: "12GB",
+        rom: "512GB",
+        price: 100999,
+        discount: 5, 
+        condition: "Superb" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 14 (Red, 128 GB)",
+        color: "Red",
+        image: "images/buy-page-phones/i-14-red.webp",
+        specs: {
+            processor: "A15 Bionic Chip,6 core Processor",
+            display: "15.49 cm (6.1 inch) Super Retina XDR Display",
+            battery: 3279,
+            camera: "12MP + 12MP | 12MP Front Camera",
+            os: "iOS 16",
+            network: "5G",
+            weight: "172g"
+        },
+        ram: "6GB",
+        rom: "128GB",
+        price: 54999,
+        discount: 8, 
         condition: "Good" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Apple",
+        model: "iPhone 12 (Blue, 64 GB)",
+        color: "Blue",
+        image: "images/buy-page-phones/i-12.webp",
+        specs: {
+            processor: "A14 Bionic Chip with Next Generation Neural Engine Processor",
+            display: "15.49 cm (6.1 inch) Super Retina XDR Display",
+            battery: 2815,
+            camera: "12MP + 12MP | 12MP Front Camera",
+            os: "iOS 14",
+            network: "5G",
+            weight: "162g"
+        },
+        ram: "4GB",
+        rom: "64GB",
+        price: 48999,
+        discount: 8, 
+        condition: "very Good" // Condition added
+    }
+    //lenovo
+    ,  {
+        id: 0,
+        brand: "Lenovo",
+        model: "K8 Note (Venom Black, 64 GB)",
+        color: "Venom Black",
+        image: "images/buy-page-phones/l-k8.webp",
+        specs: {
+            processor: "Mediatek MTK X23",
+            display: "13.97 cm (5.5 inch) IPS Display",
+            battery: 4000,
+            camera: "13MP + 5MP | 13MP Front Camera",
+            os: "Android Nougat 7.1",
+            network: "4G",
+            weight: "182g"
+        },
+        ram: "4GB",
+        rom: "64GB",
+        price: 12999,
+        discount: 10, 
+        condition: "very Good" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Lenovo",
+        model: "K8 Note (Gold, 32 GB)",
+        color: "Gold",
+        image: "images/buy-page-phones/l-k8-gold.webp",
+        specs: {
+            processor: "Mediatek MTK X23",
+            display: "13.97 cm (5.5 inch) IPS Display",
+            battery: 4000,
+            camera: "13MP + 5MP | 13MP Front Camera",
+            os: "Android Nougat 7.1",
+            network: "4G",
+            weight: "182g"
+        },
+        ram: "3GB",
+        rom: "32GB",
+        price: 7999,
+        discount: 10, 
+        condition: "very Good" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Lenovo",
+        model: "A6 Note  (Blue, 32 GB)",
+        color: "Blue",
+        image: "images/buy-page-phones/l-a6.webp",
+        specs: {
+            processor: "MediaTek P22 Octa Core Processor",
+            display: "15.5 cm (6.102 inch) HD+ Display",
+            battery: 4000,
+            camera: "13MP + 2MP | 5MP Front Camera",
+            os: "Android Pie 9.0",
+            network: "4G",
+            weight: "182g"
+        },
+        ram: "3GB",
+        rom: "32GB",
+        price: 9999,
+        discount: 10, 
+        condition: "Good" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Lenovo",
+        model: "Vibe K5 Plus (Gold, 16 GB)",
+        color: "Gold",
+        image: "images/buy-page-phones/l-k5plus.webp",
+        specs: {
+            processor: "Snapdragon 616 Octa Core 1.5GHz Processor",
+            display: "12.7 cm (5 inch) Full HD Display",
+            battery: 2750,
+            camera: "13MP  | 5MP Front Camera",
+            os: "Android Pie 9.0",
+            network: "3G",
+            weight: "142g"
+        },
+        ram: "3GB",
+        rom: "16GB",
+        price: 7999,
+        discount: 2, 
+        condition: "Good" // Condition added
+    }
+    ,  {
+        id: 0,
+        brand: "Lenovo",
+        model: "K9 (Black, 32 GB)",
+        color: "Black",
+        image: "images/buy-page-phones/l-k9.webp",
+        specs: {
+            processor: "MediaTek P22 Processor",
+            display: "14.7 cm (5.5 inch) Full HD Display",
+            battery: 3000,
+            camera: "13MP  +5MP | 13MP+5MP Dual Front Camera",
+            os: "Android Pie 9.0",
+            network: "3G",
+            weight: "155g"
+        },
+        ram: "3GB",
+        rom: "32GB",
+        price: 9999,
+        discount: 3, 
+        condition: "Very Good" // Condition added
     }
 ];

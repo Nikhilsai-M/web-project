841c4d31df17fa387f3c9794e66ce84e0564e345
Abhinav <abhinavysns03@gmail.com>
2025-03-12 14:13:00 +0530

    One Plus and Google data added for buy phones

--- public/scripts/buy-mobile-data.js
+++ public/scripts/buy-mobile-data.js
@@ -1,65 +1,443 @@
 const mobilePhones = [
+// OnePlus
     {
-        id: 1,
-        brand: "Apple",
-        model: "iPhone 14",
-        color: "Midnight",
-        image: "images/buy-page-phones/image_name",
-        specs: {
-            processor: "A15 Bionic",
-            display: "6.1-inch Super Retina XDR",
-            battery: 3279,
-            camera: "12MP + 12MP | 12MP Front Camera",
-            os: "iOS 16",
-            network: "5G",
-            weight: "172g"
-        },
-        ram: "4GB",
-        rom: "128GB",
-        price: 79999,
-        discount: 10, // 10% off
-        condition: "Superb" // Condition added
+id: ,
+brand: "OnePlus",
+model: "OnePlus Nord 2",
+color: "Blue Haze",
+image: "/images/buy-page-phones/OnePlus/img1.webp",
+specs: {
+    processor: "Snapdragon 8 Gen 1",
+    display: "6.43-inch Fluid AMOLED display",
+    battery: 4500,
+    camera: "48MP + 8MP + 50MP | 32MP Front Camera",
+    os: "Android 11",
+    network: "5G",
+    weight: "200g"
+},
+ram: "12GB",
+rom: "256GB",
+price: 18399,
+discount: 15, // 15% off
+condition: "Good" // Condition added
+},
+
+{
+    id: ,
+    brand: "OnePlus",
+    model: " OnePlus 8T 5G",
+    color: "Lunar Silver",
+    image: "/images/buy-page-phones/OnePlus/img2.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.55-inch Fluid AMOLED display",
+        battery: 4500,
+        camera: "48MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 11",
+        network: "5G",
+        weight: "200g"
     },
+    ram: "8GB",
+    rom: "128GB",
+    price: 19999,
+    discount: 53, 
+    condition: "Good" 
+},
+
+{
+    id: ,
+    brand: "OnePlus",
+    model: " OnePlus 10T 5G",
+    color: "Jade Green",
+    image: "/images/buy-page-phones/OnePlus/img3.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.7-inch Fluid AMOLED display",
+        battery: 4800,
+        camera: "48MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 12",
+        network: "5G",
+        weight: "210g"
+    },
+    ram: "12GB",
+    rom: "256GB",
+    price: 28999,
+    discount: 47, 
+    condition: "Superb" 
+},
+
+{
+    id: ,
+    brand: "OnePlus",
+    model: " OnePlus 11 5G",
+    color: "Eternal Green",
+    image: "/images/buy-page-phones/OnePlus/img4.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.7-inch Fluid AMOLED display",
+        battery: 5000,
+        camera: "48MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 12",
+        network: "5G",
+        weight: "220g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 32999,
+    discount: 13, 
+    condition: "Superb" 
+},
+{
+    id: ,
+    brand: "OnePlus",
+    model: " OnePlus 8T 5G",
+    color: "AquaMarine Green",
+    image: "/images/buy-page-phones/OnePlus/img5.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.55-inch Fluid AMOLED display",
+        battery: 4500,
+        camera: "48MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 11",
+        network: "5G",
+        weight: "200g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 21499,
+    discount: 53, 
+    condition: "Good" 
+},
+{
+    id: ,
+    brand: "OnePlus",
+    model: "OnePlus Nord 2",
+    color: "Gray Sierra",
+    image: "/images/buy-page-phones/OnePlus/img6.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.43-inch Fluid AMOLED display",
+        battery: 4500,
+        camera: "48MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 11",
+        network: "5G",
+        weight: "200g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 16799,
+    discount: 52, // 15% off
+    condition: "Good" // Condition added
+    },
+
+{
+    id: ,
+    brand: "OnePlus",
+    model: "OnePlus 10R",
+    color: "Sierra Black",
+    image: "/images/buy-page-phones/OnePlus/img7.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.7-inch Fluid AMOLED display",
+        battery: 5000,
+        camera: "48MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 12",
+        network: "5G",
+        weight: "220g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 21999,
+    discount: 43, // 15% off
+    condition: "Superb" // Condition added
+},
+
+{
+    id: ,
+    brand: "OnePlus",
+    model: "OnePlus 10R",
+    color: "Forest Green",
+    image: "/images/buy-page-phones/OnePlus/img8.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.7-inch Fluid AMOLED display",
+        battery: 5000,
+        camera: "48MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 12",
+        network: "5G",
+        weight: "220g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 21999,
+    discount: 43, // 15% off
+    condition: "Superb" // Condition added
+},
+        
+{
+    id: ,
+    brand: "OnePlus",
+    model: "OnePlus 11R",
+    color: "Sonic Black",
+    image: "/images/buy-page-phones/OnePlus/img9.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.7-inch Fluid AMOLED display",
+        battery: 5000,
+        camera: "48MP + 8MP + 50MP | 16MP Front Camera",
+        os: "Android 13",
+        network: "5G",
+        weight: "210g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 24499,
+    discount: 45, // 15% off
+    condition: "Superb" // Condition added
+},
+
+{
+    id: ,
+    brand: "OnePlus",
+    model: "OnePlus  Nord CE 3 Lite",
+    color: "Pastel Lime",
+    image: "/images/buy-page-phones/OnePlus/img10.webp",
+    specs: {
+        processor: "Snapdragon 8 Gen 1",
+        display: "6.72-inch Fluid AMOLED display",
+        battery: 5000,
+        camera: "50MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 13",
+        network: "5G",
+        weight: "200g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 12999,
+    discount: 40, // 15% off
+    condition: "Superb" // Condition added
+},
+// Google
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 7",
+    color: "Lemon Grass",
+    image: "/images/buy-page-phones/Google/img1.webp",
+    specs: {
+    processor: "Google Tensor G2",
+    display: "6.3-inch Full HD Plus display",
+    battery: 4270,
+    camera: "50MP + 8MP + 50MP | 32MP Front Camera",
+    os: "Android 13",
+    network: "5G",
+    weight: "197g"
+   },
+    ram: "8GB",
+    rom: "128GB",
+    price: 33999,
+    discount: 55, // 15% off
+    condition: "Superb" // Condition added
+},
+
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 7",
+    color: "Obsidian",
+    image: "/images/buy-page-phones/Google/img2.webp",
+    specs: {
+        processor: "Google Tensor G2",
+        display: "6.3-inch Full HD Plus display",
+        battery: 4270,
+        camera: "50MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 13",
+        network: "5G",
+        weight: "197g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 33999,
+    discount: 55, // 15% off
+    condition: "Superb" // Condition added
+    },
+
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 7a",
+    color: "Charcoal",
+    image: "/images/buy-page-phones/Google/img3.webp",
+    specs: {
+        processor: "Google Tensor G2",
+        display: "6.1-inch Full HD Plus display",
+        battery: 4300,
+        camera: "58MP + 8MP + 50MP | 13MP Front Camera",
+        os: "Android 13",
+        network: "5G",
+        weight: "193.5g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 29499,
+    discount: 32, // 15% off
+    condition: "Superb" // Condition added
+},
+
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 7a",
+    color: "Sea",
+    image: "/images/buy-page-phones/Google/img4.webp",
+    specs: {
+        processor: "Google Tensor G2",
+        display: "6.1-inch Full HD Plus display",
+        battery: 4300,
+        camera: "58MP + 8MP + 50MP | 13MP Front Camera",
+        os: "Android 13",
+        network: "5G",
+        weight: "193.5g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 29499,
+    discount: 32, // 15% off
+    condition: "Superb" // Condition added
+},
+
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 7a",
+    color: "Snow",
+    image: "/images/buy-page-phones/Google/img5.webp",
+    specs: {
+        processor: "Google Tensor G2",
+        display: "6.1-inch Full HD Plus display",
+        battery: 4300,
+        camera: "58MP + 8MP + 50MP | 13MP Front Camera",
+        os: "Android 13",
+        network: "5G",
+        weight: "193.5g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 29499,
+    discount: 32, // 15% off
+    condition: "Superb" // Condition added
+},
+
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 7",
+    color: "Snow",
+    image: "/images/buy-page-phones/Google/img6.webp",
+    specs: {
+        processor: "Google Tensor G2",
+        display: "6.3-inch Full HD Plus display",
+        battery: 4270,
+        camera: "50MP + 8MP + 50MP | 32MP Front Camera",
+        os: "Android 13",
+        network: "5G",
+        weight: "197g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 33999,
+    discount: 43, // 15% off
+    condition: "Superb" // Condition added
+    },
+
     {
-        id: 2,
-        brand: "Samsung",
-        model: "Galaxy S22",
-        color: "Phantom Black",
-        image: "images/buy-page-phones/image_name",
+        id: ,
+        brand: "Google",
+        model: "Google Pixel 8",
+        color: "Hazel",
+        image: "/images/buy-page-phones/Google/img7.webp",
         specs: {
-            processor: "Snapdragon 8 Gen 1",
-            display: "6.1-inch Dynamic AMOLED",
-            battery: 3700,
-            camera: "50MP + 10MP + 12MP | 10MP Front Camera",
-            os: "Android 12",
+            processor: "Google Tensor G3",
+            display: "6.2-inch Full HD Plus display",
+            battery: 4575,
+            camera: "50MP + 8MP + 50MP | 10.5MP Front Camera",
+            os: "Android 14",
             network: "5G",
-            weight: "167g"
+            weight: "187g"
         },
         ram: "8GB",
-        rom: "128GB",
-        price: 69999,
-        discount: 5, // 5% off
-        condition: "Very Good" // Condition added
-    },
-    {
-        id: 3,
-        brand: "OnePlus",
-        model: "OnePlus 10 Pro",
-        color: "Emerald Green",
-        image: "images/buy-page-phones/image_name",
-        specs: {
-            processor: "Snapdragon 8 Gen 1",
-            display: "6.7-inch Fluid AMOLED",
-            battery: 5000,
-            camera: "48MP + 8MP + 50MP | 32MP Front Camera",
-            os: "Android 12",
-            network: "5G",
-            weight: "200g"
-        },
-        ram: "12GB",
         rom: "256GB",
-        price: 71999,
-        discount: 15, // 15% off
-        condition: "Good" // Condition added
-    }
+        price: 58999,
+        discount: 26, // 15% off
+        condition: "Superb" // Condition added
+        },
+    
+    
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 8",
+    color: "Mint",
+    image: "/images/buy-page-phones/Google/img8.webp",
+    specs: {
+        processor: "Google Tensor G3",
+        display: "6.2-inch Full HD Plus display",
+        battery: 4575,
+        camera: "50MP + 8MP + 50MP | 10.5MP Front Camera",
+        os: "Android 14",
+        network: "5G",
+        weight: "187g"
+    },
+    ram: "8GB",
+    rom: "128GB",
+    price: 52999,
+    discount: 45, // 15% off
+    condition: "Superb" // Condition added
+},
+
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 8 Pro",
+    color: "Bay",
+    image: "/images/buy-page-phones/Google/img9.webp",
+    specs: {
+        processor: "Google Tensor G3",
+        display: "6.7-inch Full HD Plus display",
+        battery: 5050,
+        camera: "50MP + 8MP + 50MP | 10.5MP Front Camera",
+        os: "Android 14",
+        network: "5G",
+        weight: "213g"
+    },
+    ram: "12GB",
+    rom: "128GB",
+    price: 84999,
+    discount: 20, // 15% off
+    condition: "Superb" // Condition added
+},
+
+{
+    id: ,
+    brand: "Google",
+    model: "Google Pixel 8 Pro",
+    color: "Obsidian",
+    image: "/images/buy-page-phones/Google/img10.webp",
+    specs: {
+        processor: "Google Tensor G3",
+        display: "6.7-inch Full HD Plus display",
+        battery: 5050,
+        camera: "50MP + 8MP + 50MP | 10.5MP Front Camera",
+        os: "Android 14",
+        network: "5G",
+        weight: "213g"
+    },
+    ram: "12GB",
+    rom: "128GB",
+    price: 84999,
+    discount: 20, // 15% off
+    condition: "Superb" // Condition added
+},
+
+
 ];
